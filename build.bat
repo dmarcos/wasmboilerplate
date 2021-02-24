9ab86b6ddae0cde07cad0fcf7aa2323931fb27fe
Diego Marcos <diego.marcos@gmail.com>
2021-02-23 23:55:57 -0500

    Remove unnecessary INCLUDE and LIB variables set

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM Use system compiler if there's one available
-SET compiler="clx"
+SET compiler="cl"
 %compiler% 0> nul 1> nul 2> nul
 IF NOT %ERRORLEVEL%==9009 GOTO :BUILD
 
@@ -38,22 +38,6 @@ CALL SET templateDir=%%templateDir:%drive%=%drivePath%%%
 if not defined DevEnvDir (
   call "%templateDir%\tools\vs\VC\Auxiliary\Build\vcvars32.bat"
 )
-
-SET "INCLUDE1=C:\Program Files (x86)\Windows Kits\NETFXSDK\4.8\include\um"
-SET "INCLUDE2=C:\Program Files (x86)\Windows Kits\10\include\10.0.17763.0\ucrt"
-SET "INCLUDE3=C:\Program Files (x86)\Windows Kits\10\include\10.0.17763.0\um"
-SET "INCLUDE4=C:\Program Files (x86)\Windows Kits\10\include\10.0.17763.0\winrt"
-SET "INCLUDE5=C:\Program Files (x86)\Windows Kits\10\include\10.0.17763.0\cppwinrt"
-SET "INCLUDE6=%templateDir%\tools\vs\VC\Tools\MSVC\14.16.27023\include"
-SET INCLUDE=%INCLUDE1%;%INCLUDE2%;%INCLUDE3%;%INCLUDE4%;%INCLUDE5%;%INCLUDE6%
-
-SET "LIB1=C:\Program Files (x86)\Windows Kits\NETFXSDK\4.8\lib\um\x86"
-SET "LIB2=C:\Program Files (x86)\Windows Kits\10\lib\10.0.17763.0\ucrt\x86"
-SET "LIB3=C:\Program Files (x86)\Windows Kits\10\lib\10.0.17763.0\um\x86"
-SET "LIB3=C:\Program Files (x86)\Windows Kits\10\lib\10.0.17763.0\um\x86"
-SET "LIB4=%templateDir%\tools\vs\VC\Tools\MSVC\14.28.27023\ATLMFC\lib\x86"
-SET "LIB5=%templateDir%\tools\vs\VC\Tools\MSVC\14.16.27023\lib\x86"
-SET LIB=%LIB1%;%LIB2%;%LIB3%;%LIB4%;%LIB5%
 
 :BUILD
 
