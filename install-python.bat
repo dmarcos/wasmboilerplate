e083a4db6416031d3027d1e5e28c9c9a737ad609
Diego Marcos <diego.marcos@gmail.com>
2021-02-25 10:33:48 -0500

    Uncommenting necessary commands

--- install-python.bat
+++ install-python.bat
@@ -18,14 +18,14 @@ SET compilerDownloadDestination="%downloadDestination%\python-3.9.2.exe"
 ECHO Downloading Python...
 
 REM Download compiler from Microsoft.
-REM CALL bitsadmin /transfer "Downloading Python" /download /priority normal %downloadURL% %compilerDownloadDestination%
+CALL bitsadmin /transfer "Downloading Python" /download /priority normal %downloadURL% %compilerDownloadDestination%
 
 ECHO Installing Python...
 
 IF NOT EXIST .\tools\python mkdir .\tools\python
 
 SET pythonDir="%currentDir%\tools\python"
-REM CALL downloads\python-3.9.2.exe /quiet TargetDir=%pythonDir%
+CALL downloads\python-3.9.2.exe /quiet TargetDir=%pythonDir%
 
 SET PATH=%pythonDir%;%PATH%
 
